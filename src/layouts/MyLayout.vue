4d727bbe789e5c9eb9af54c1a1d294cf6a67dac6
mariana.ruddy <mariana.ruddy@sieve.com.br>
2020-01-08 21:19:48 -0300

    initial commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+        <div class="q-ps-lg q-pt-xl q-nb-md">
+          <div class="text-h3">Todo</div>
+          <div class="text-hsbtitle1">{{ todaysDate }}</div>
+        </div>
+        <q-img src="statics/mountains.jpg" class="header-image absolute-top" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="250"
+      :breakpoint="600"
+    >
+      <q-scroll-area
+        style="height: calc(100% - 119px); margin-top: 119px; border-right: 1px solid #ddd"
+      >
+        <q-list padding>
+          <q-item exact to="/" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section>
+              Todo
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <q-list padding>
+          <q-item exact to="/help" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>
+              Help
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="statics/mountains.jpg"
+        style="height: 119px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://s.gravatar.com/avatar" />
+          </q-avatar>
+          <div class="text-weight-bold">Mariana Ruddy</div>
+          <div>@marianaruddy</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from "quasar";
+export default {
+  name: "MyLayout",
+
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  },
+  computed: {
+    todaysDate() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd D MMMM");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
